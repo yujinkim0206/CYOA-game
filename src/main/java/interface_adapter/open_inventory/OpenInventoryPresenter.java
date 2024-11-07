c7d066223d02b56364db62c99bedd6cd56e64973
eMondri <161959103+eMondri-UofT@users.noreply.github.com>
2024-11-06 22:13:19 -0500

    Trying to get maven working for testing + updated presenter with placeholder classes called 'RoomDefault_

--- src/main/java/interface_adapter/open_inventory/OpenInventoryPresenter.java
+++ src/main/java/interface_adapter/open_inventory/OpenInventoryPresenter.java
@@ -1,9 +1,8 @@
 package interface_adapter.open_inventory;
 
 import interface_adapter.ViewManagerModel;
-// Import view model of screen being transitioned to
-//import interface_adapter.___;
-import interface_adapter.change_password.LoggedInState;
+import interface_adapter.room_default.RoomDefaultViewModel;
+import interface_adapter.room_default.RoomDefaultState;
 import use_case.open_inventory.OpenInventoryOutputBoundary;
 import use_case.open_inventory.OpenInventoryOutputData;
 
@@ -15,22 +14,29 @@ public class OpenInventoryPresenter implements OpenInventoryOutputBoundary {
     private OpenInventoryViewModel openInventoryViewModel;
     private ViewManagerModel viewManagerModel;
 //    private LoginViewModel loginViewModel;   REPLACE THIS WITH THE SCREEN BEING TRANSITIONED TO
+    private RoomDefaultViewModel roomDefaultViewModel;
 
     public OpenInventoryPresenter(ViewManagerModel viewManagerModel,
-                          OpenInventoryViewModel openInventoryViewModel
-                           /*LoginViewModel loginViewModel  REPLACE THIS WITH THE SCREEN BEING TRANSITIONED TO*/) {
+                          OpenInventoryViewModel openInventoryViewModel,
+                           /*LoginViewModel loginViewModel  REPLACE THIS WITH THE SCREEN BEING TRANSITIONED TO*/
+                          RoomDefaultViewModel roomDefaultViewModel) {
         this.openInventoryViewModel = openInventoryViewModel;
         this.viewManagerModel = viewManagerModel;
         //this.???ViewModel = ???ViewModel;   REPLACE THIS WITH THE SCREEN BEING TRANSITIONED TO
+        this.roomDefaultViewModel = roomDefaultViewModel;
     }
 
     @Override
     public void prepareSuccessView(OpenInventoryOutputData response) {
 //        final LoggedInState loggedInState = loggedInViewModel.getState();   REPLACE W/ CORRECT SCREEN
+        final RoomDefaultState roomDefaultState = roomDefaultViewModel.getState();
 //        this.loggedInViewModel.setState(loggedInState);   REPLACE W/ CORRECT SCREEN
+        this.roomDefaultViewModel.setState(roomDefaultState);
 //        this.loggedInViewModel.firePropertyChanged();   REPLACE W/ CORRECT SCREEN
+        this.roomDefaultViewModel.firePropertyChanged();
 
-        this.viewManagerModel.setState(/* loggedInViewModel.getViewName() REPLACE W/ CORRECT SCREEN*/);
+        this.viewManagerModel.setState(/* loggedInViewModel.getViewName() REPLACE W/ CORRECT SCREEN*/
+                                       roomDefaultViewModel.getViewName());
         this.viewManagerModel.firePropertyChanged();
     }
 
---
+++ src/main/java/interface_adapter/room_default/RoomDefaultState.java
@@ -0,0 +1,11 @@
+package interface_adapter.room_default;
+
+/**
+ * The State information representing the room default.
+ */
+public class RoomDefaultState {
+
+    public RoomDefaultState() {
+
+    }
+}
