b5b29c283853f5775f730e3449dfe0ed8931a917
Yujin Kim <yuj.kim@mail.utoronto.ca>
2024-11-07 21:51:20 -0500

    added entities Inventory, InventoryFactory, and Item

---
+++ src/main/java/entity/InventoryFactory.java
@@ -0,0 +1,15 @@
+package java.entity;
+
+/**
+ * Factory for creating Inventory.
+ */
+public class InventoryFactory {
+
+    /**
+     * Creates a new Inventory.
+     * @return the new Inventory
+     */
+    public Inventory create() {
+        return new Inventory();
+    }
+}
