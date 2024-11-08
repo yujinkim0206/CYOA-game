cf4e41fa8ba4045bbf92f55bb582fbe4570fa2b5
samqyz <samqy.zheng@mail.utoronto.ca>
2024-11-08 16:22:07 -0500

    Created the Floor and Room entities and moved to entity

---
+++ src/main/java/entity/NPCRoom.java
@@ -0,0 +1,32 @@
+package entity;
+
+public class NPCRoom extends Room{
+    private Npc npc;
+
+    /**
+     * Creates an NPCRoom
+     * @param npc the Npc in the NPCRoom
+     */
+
+    public NPCRoom(Npc npc){
+        this.npc = npc;
+    }
+
+    /**
+     * Returns the Npc in the NPCRoom
+     * @return the Npc in NPCRoom
+     */
+
+    public Npc getNpc(){
+        return npc;
+    }
+
+    /**
+     * Allows us to set the Npc in the NPCRoom
+     * @param npc the npc we want to set inside the NPCRoom
+     */
+
+    public void setNpc(Npc npc){
+        this.npc = npc;
+    }
+}
