e26f903eb877e684a05879cbe4a37c2d8a77cf10
Yujin Kim <yuj.kim@mail.utoronto.ca>
2024-11-30 15:32:31 -0500

    entity tests completed

---
+++ src/test/java/Entitiy_Test/TrapTest.java
@@ -0,0 +1,35 @@
+package Entitiy_Test;
+
+import entity.Trap;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TrapTest {
+
+    private Trap trap;
+
+    @BeforeEach
+    public void setUp() {
+        trap = new Trap("Pendulums", 10);
+    }
+
+    @Test
+    void testTrapCreation() {
+        assertEquals("Pendulums", trap.getName());
+        assertEquals(10, trap.getDamage());
+    }
+
+    @Test
+    void testSetName() {
+        trap.setName("Fire Trap");
+        assertEquals("Fire Trap", trap.getName());
+    }
+
+    @Test
+    void testSetDamage() {
+        trap.setDamage(25);
+        assertEquals(25, trap.getDamage());
+    }
+}
